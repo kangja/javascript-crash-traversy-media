d396f0bf10c8872b2a4cd2f691f3ab145752d3bd
Jason <kangja1996@gmail.com>
2021-04-16 21:37:15 -0400

    progress

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-const fruits = ["apples", "oranges", "pears"];
+const todos = [
+  {
+    id: 1,
+    text: "Take out trash",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    text: "Meeting with boss",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    text: "Dentist Appointment",
+    isCompleted: false,
+  },
+];
 
-fruits[3] = "grapes";
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`For Loop Number: ${i}`);
+// }
 
-fruits.push("mangos");
-
-fruits.unshift("straberries");
-
-fruits.pop();
-
-// console.log(Array.isArray("oranges"));
-
-console.log(fruits.indexOf("oranges"));
-
-console.log(fruits);
+let i = 0;
+while (i <= 10) {
+  console.log(`While Loop Number: ${i}`);
+  i++;
+}
