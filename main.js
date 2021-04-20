4436dd62c99ee493660d501864f6ee386d83d43e
Jason <kangja1996@gmail.com>
2021-04-20 06:34:57 -0400

    progress

--- main.js
+++ main.js
@@ -1,43 +1,6 @@
-const todos = [
-  {
-    id: 1,
-    text: "Take out trash",
-    isCompleted: true,
-  },
-  {
-    id: 2,
-    text: "Meeting with boss",
-    isCompleted: true,
-  },
-  {
-    id: 3,
-    text: "Dentist Appointment",
-    isCompleted: false,
-  },
-];
+const x = 6;
+const y = 11;
 
-// for (let i = 0; i < todos.length; i++) {
-//   console.log(todos[i].text);
-// }
-
-// for (let todo of todos) {
-//   console.log(todo.text);
-// }
-
-// for (let d of todos) {
-//     console.log(d);
-//   }
-
-// todos.forEach(function (a) {
-//   console.log(a.text);
-// });
-
-const todoCompleted = todos
-  .filter(function (todo) {
-    return todo.isCompleted === true;
-  })
-  .map(function (todo) {
-    return todo.text;
-  });
-
-console.log(todoCompleted);
+if (x > 5 && y > 10) {
+  console.log("x is more than 5 AND y is more than 10");
+}
