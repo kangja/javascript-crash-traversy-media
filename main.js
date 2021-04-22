072648bf3eddfdd6f96eb7d73b152fbeab9dfa06
Jason <kangja1996@gmail.com>
2021-04-22 11:23:58 -0400

    progress

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const x = 0;
-
-const color = "green";
-
-switch (color) {
-  case "red":
-    console.log("color is red");
-    break;
-  case "blue":
-    console.log("color is blue");
-    break;
-  default:
-    console.log("color is NOT red or blue");
-    break;
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob);
+  this.getBirthYear = function () {
+    return this.dob.getFullYear();
+  };
+  this.getFullName = function () {
+    return `${this.firstName} ${this.lastName}`;
+  };
 }
+
+const person1 = new Person("John", "Doe", "7-29-1995");
+
+const person2 = new Person("Jason", "Kang", "8-1-1996");
+
+console.log(person1.getBirthYear());
+console.log(person2.getFullName());
