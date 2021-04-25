8a1d4fa6f998eb73036f5648c60ef83f01eb9f99
Jason <kangja1996@gmail.com>
2021-04-25 15:15:32 -0400

    progress

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dob = new Date(dob);
-}
+console.log(document.getElementById("my-form"));
 
-Person.prototype.getBirthYear = function () {
-  return this.dob.getFullYear();
-};
-
-Person.prototype.getFullName = function () {
-  return `${this.firstName} ${this.lastName}`;
-};
-
-class Person {
-  constructor(firstName, lastName, dob) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.dob = new Date(dob);
-  }
-
-  getBirthYear() {
-    return this.dob.getFullYear();
-  }
-
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-
-const person1 = new Person("John", "Doe", "7-29-1995");
-
-const person2 = new Person("Jason", "Kang", "8-1-1996");
-
-console.log(person2.getFullName());
-console.log(person2);
+console.log(document.querySelector("h1"));
