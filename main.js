0a57de22dc6f44b592bf9c3284d24636225ee145
Jason <kangja1996@gmail.com>
2021-04-25 15:42:48 -0400

    progress

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-console.log(document.getElementById("my-form"));
+// console.log(document.getElementById("my-form"));
 
-console.log(document.querySelector("h1"));
+// console.log(document.querySelector("h1"));
+
+// console.log(document.querySelectorAll(".item"));
+
+// console.log(document.getElementsByClassName("item"));
+
+console.log(document.getElementsByTagName("li"));
