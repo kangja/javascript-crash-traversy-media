5a135d94bed3c9d2a6a65b3eada1b502c4a2e0cb
Jason <kangja1996@gmail.com>
2021-04-29 12:58:38 -0400

    progress

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const btn = document.querySelector(".btn");
+
+btn.addEventListener("mouseout", (e) => {
+  e.preventDefault();
+  document.querySelector("#my-form").style.background = "#ccc";
+
+  document.querySelector("body").classList.add("bg-dark");
+
+  document.querySelector(".items").lastElementChild.innerHTML =
+    "<h1>TO THE MOON</h1>";
+});
